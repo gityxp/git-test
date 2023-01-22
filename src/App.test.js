d547621a5f2be3ba9fade755284285954ad5d9b5
yxp <2806617834@qq.com>
2023-01-22 11:23:30 +0800

    gyxp commit

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,8 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 import Footer from './pages/Footer/index.jsx'
 import Header from './pages/Header/index.jsx'
+import Item from './pages/Item/index.jsx'
+
 
 test('renders learn react link', () => {
   render(<App />);
